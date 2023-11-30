8099c0458b8b3653348b0a366c61bc9427212723
Ingmar Falk <if222gq@student.lnu.se>
2023-11-30 10:11:50 +0100

    GREEN: exec errors on invalid argument

--- src/cmd.rs
+++ src/cmd.rs
@@ -2,10 +2,16 @@
 pub enum ExecError {
     #[error("No arguments provided")]
     NoArgs,
+
+    #[error("Invalid argument: {0}")]
+    InvalidArg(String),
 }
 
 pub fn exec(args: &[String]) -> Result<(), ExecError> {
-    Err(ExecError::NoArgs)
+    match args.len() {
+        0 => Err(ExecError::NoArgs),
+        _ => Err(ExecError::InvalidArg(args[0].clone())),
+    }
 }
 
 #[cfg(test)]
@@ -24,6 +30,9 @@ pub mod tests {
         let args = vec!["foo".to_string(), "bar".to_string()];
         let result = exec(&args);
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), ExecError::InvaliArgument("foo".into()));
+        assert_eq!(
+            result.unwrap_err(),
+            ExecError::InvalidArg("foo".to_string())
+        );
     }
 }
