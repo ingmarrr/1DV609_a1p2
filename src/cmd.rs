e34f165e02226b57a00e05eb63c219eada3b0794
Ingmar Falk <if222gq@student.lnu.se>
2023-11-30 11:10:16 +0100

    RED: exec should write to out writer

--- src/cmd.rs
+++ src/cmd.rs
@@ -32,7 +32,7 @@ pub fn exec(args: &[String]) -> Result<(), ExecError> {
         .find(|c| c.name == cmd)
         .ok_or_else(|| ExecError::InvalidArg(cmd.to_string()))?;
 
-    ExecError::NoArgs
+    (cmd.func)(args)
 }
 
 #[cfg(test)]
@@ -48,19 +48,25 @@ pub mod tests {
 
     #[test]
     fn exec_should_err_on_invalid_args() {
-        let args = vec!["foo".to_string(), "bar".to_string()];
+        let args = vec!["foo".into(), "bar".into()];
         let result = exec(&args);
         assert!(result.is_err());
-        assert_eq!(
-            result.unwrap_err(),
-            ExecError::InvalidArg("foo".to_string())
-        );
+        assert_eq!(result.unwrap_err(), ExecError::InvalidArg("foo".into()));
     }
 
     #[test]
     fn exec_fn_should_ok_on_valid_arg() {
-        let args = vec!["help".to_string()];
+        let args = vec!["help".into()];
         let result = exec(&args);
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn exec_should_write_to_out_writer() {
+        let args = vec!["help".into()];
+        let writer = StdoutWriter::new();
+        let result = exec(&args, &writer);
+        assert!(result.is_ok());
+        assert_eq!(writer.get(), usage());
     }
 }
