fae9f3ce520b72a4bd641811277e719f348ec31c
Ingmar Falk <if222gq@student.lnu.se>
2023-11-30 10:03:33 +0100

    RED: exec should err on empty

---
+++ src/cmd.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn exec_should_err_on_empty() {
+        let args = vec![];
+        let result = exec(args);
+        assert!(result.is_err());
+    }
+}
