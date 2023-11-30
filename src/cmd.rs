1bc60c89c25a0e1bfcd3add30a0b0834e752721b
Ingmar Falk <if222gq@student.lnu.se>
2023-11-30 10:09:07 +0100

    RED: exec should err on invalid argument

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,4 +1,4 @@
-#[dervie(Debug, thiserror::Error, PartialEq, Eq)]
+#[derive(Debug, thiserror::Error, PartialEq, Eq)]
 pub enum ExecError {
     #[error("No arguments provided")]
     NoArgs,
@@ -17,5 +17,13 @@ pub mod tests {
         let args = vec![];
         let result = exec(&args);
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn exec_should_err_on_invalid_args() {
+        let args = vec!["foo".to_string(), "bar".to_string()];
+        let result = exec(&args);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), ExecError::InvaliArgument("foo".into()));
     }
 }
