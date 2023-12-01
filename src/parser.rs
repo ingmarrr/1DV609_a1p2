45aef9abde5c2811f951c83022e648ecd6f821c6
Ingmar Falk <if222gq@student.lnu.se>
2023-12-01 15:49:05 +0100

    RED: parser should ok on empty input

--- src/parser.rs
+++ src/parser.rs
@@ -1,2 +1,12 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn parse_should_ok_on_empty() {
+        let mut parse = Parser::new("");
+        let result = parse.parse();
+        assert_eq!(result, Ok(vec![]));
+    }
+}
 
