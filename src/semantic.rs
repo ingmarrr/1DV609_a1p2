6ca8e24723aeac20ab90be53b606b940a6ee4d94
Ingmar Falk <if222gq@student.lnu.se>
2023-12-05 17:46:38 +0100

    GREEN: semantics check fails for add operation between int and string

--- src/semantic.rs
+++ src/semantic.rs
@@ -1,11 +1,30 @@
+use crate::parser::Prog;
+
+pub struct Semantic {
+    prog: Prog,
+}
+
+impl Semantic {
+    pub fn new(prog: Prog) -> Self {
+        Self { prog }
+    }
+
+    pub fn check(&self) -> Result<(), String> {
+        return Err("Not implemented".to_string());
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
+    use crate::parser::Parser;
+
     use super::*;
 
     #[test]
     fn semantics_check_should_fail_for_unsupported_operators() {
-        let mut ast = parser::parse("1 + \"hello\"");
-        let result = semantic::check(&mut ast);
+        let mut parser = Parser::new("1 + \"hello\"").unwrap();
+        let prog = parser.parse().unwrap();
+        let result = Semantic::new(prog).check();
         assert!(result.is_err());
     }
 }
