67ea00f2ba1e35b721193f08a11e5acc6e3b14f2
Ingmar Falk <if222gq@student.lnu.se>
2023-12-06 18:12:18 +0100

    GREEN: evaluator should return empty output on empty input

--- src/eval.rs
+++ src/eval.rs
@@ -1,11 +1,27 @@
+use crate::parser::Prog;
+
+pub struct Evaluator;
+
+impl Evaluator {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn eval(&mut self, prog: &Prog) -> Result<Vec<String>, String> {
+        Ok(vec![])
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
+    use crate::parser::Prog;
+
     use super::*;
 
     #[test]
     fn eval_with_empty_input_should_return_empty_output() {
         let mut eval = Evaluator::new();
-        let prog = Program { body: vec![] };
+        let prog = Prog { body: vec![] };
         let res = eval.eval(&prog);
         assert_eq!(res, Ok(vec![]));
     }
